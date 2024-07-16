18b3900b354182529a3486b560ce2b48e30a2c97
vod9hc <84865169+vod9hc@users.noreply.github.com>
2024-07-16 15:22:36 +0700

    Add files via upload

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -5,8 +5,8 @@ import logging
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager # type: ignore
-from flask_session import Session # type: ignore
+from flask_login import LoginManager
+from flask_session import Session
 
 app = Flask(__name__)
 app.config.from_object(Config)
