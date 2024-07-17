e05d019941c7d75eefeab5d873ce71c950c9ec88
vod9hc <84865169+vod9hc@users.noreply.github.com>
2024-07-17 11:59:41 +0700

    Add files via upload
    update code

--- FlaskWebProject/models.py
+++ FlaskWebProject/models.py
@@ -58,9 +58,11 @@ class Post(db.Model):
             Randomfilename = id_generator();
             filename = Randomfilename + '.' + fileextension;
             try:
-                blob_service.create_blob_from_stream(blob_container, filename, file)
+                blob_client = blob_service.get_blob_client(container=blob_container, blob=filename)
+                blob_client.upload_blob(file)
                 if(self.image_path):
-                    blob_service.delete_blob(blob_container, self.image_path)
+                    blob_client = blob_service.get_blob_client(container=blob_container, blob=self.image_path)
+                    blob_client.delete_blob()
             except Exception:
                 flash(Exception)
             self.image_path =  filename
